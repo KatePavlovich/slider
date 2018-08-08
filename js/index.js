64f426094e9293e04482135622c3f2073b7087a7
KatePavlovich <asylumhere@mail.ru>
2018-08-08 12:43:49 +0300

    rewrite with constructor

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-let slider1 = sliderFactory.createNewSlider();
-let slider2 = sliderFactory.createNewSlider();
+let slider1 = new Slider();
+let slider2 = new Slider();
 
 slider1.start('slider1');
 slider2.start('slider2');
--- js/js.js
+++ js/js.js
@@ -1,46 +1,40 @@
-let sliderFactory = {
-  createNewSlider: function(){
-    let newSlider = {
-      prevButton: null,
-      nextButton: null,
-      img: null,
-      imgArr: ['https://motivationping.com/wp-content/uploads/2017/10/motivational-inspirational-quotes-30.jpg',
+function Slider() {
+  this.prevButton = null;
+  this.nextButton = null;
+  this.img = null;
+  this.imgArr = ['https://motivationping.com/wp-content/uploads/2017/10/motivational-inspirational-quotes-30.jpg',
                   'http://rishikajain.com/wp-content/uploads/2017/07/create-yourself.jpg',
-                  'https://mk0ryrobg0ysk5t06n.kinstacdn.com/wp-content/uploads/2017/09/Hustle-Quotes-Motivation_-Don%E2%80%99t-stay-in-bed-unless-you-can.jpg'],
-      index: 0,
+                  'https://mk0ryrobg0ysk5t06n.kinstacdn.com/wp-content/uploads/2017/09/Hustle-Quotes-Motivation_-Don%E2%80%99t-stay-in-bed-unless-you-can.jpg'];
+  this.index = 0;
 
-start: function(elId) {
-  let that = this;
-  let el = document.querySelector(`#${elId}`);
-  this.prevButton = el.querySelector('.prevButton');
-  this.nextButton = el.querySelector('.nextButton');
-  this.img = el.querySelector('img');
+  this.start = function(elId) {
+    let that = this;
+    let el = document.querySelector(`#${elId}`);
+    this.prevButton = el.querySelector('.prevButton');
+    this.nextButton = el.querySelector('.nextButton');
+    this.img = el.querySelector('img');
 
-  this.prevButton.addEventListener('click', (e) => that.showPrevImage(e));
-  this.nextButton.addEventListener('click',(e) => that.showNextImage(e));
-  this.prevButton.disabled = true;
-},
-
-showPrevImage: function (e) {
-  this.index--;
-  this.img.src = this.imgArr[this.index];
-  this.nextButton.disabled = false;
-
-  if (this.index === 0) {
+    this.prevButton.addEventListener('click', (e) => that.showPrevImage(e));
+    this.nextButton.addEventListener('click',(e) => that.showNextImage(e));
     this.prevButton.disabled = true;
-  }
-},
+  };
+  this.showPrevImage = function (e) {
+    this.index--;
+    this.img.src = this.imgArr[this.index];
+    this.nextButton.disabled = false;
 
- showNextImage: function(e) {
-  this.index++;
-  this.img.src = this.imgArr[this.index];
-  this.prevButton.disabled = false;
-
-  if (this.index === this.imgArr.length-1) {
-    this.nextButton.disabled = true;
-  }
-}
+    if (this.index === 0) {
+      this.prevButton.disabled = true;
     }
-    return newSlider;
-  }
+  };
+
+  this.showNextImage = function(e) {
+    this.index++;
+    this.img.src = this.imgArr[this.index];
+    this.prevButton.disabled = false;
+
+    if (this.index === this.imgArr.length-1) {
+      this.nextButton.disabled = true;
+    }
+  };
 }
